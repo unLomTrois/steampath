2deb535a2d022f814ab26b82591b02656c653dd4
unlomtrois <vadim.kuz02@gmail.com>
2024-10-03 22:01:37 +0300

    ci: :green_heart: make eslint use nodejs globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default [
     { files: ["**/*.{js,mjs,cjs,ts}"] },
-    { languageOptions: { globals: globals.browser } },
+    { languageOptions: { globals: globals.node } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
 ];
